8a5516b0cedfc4c701123db2929dd2302e834550
Zastenchivyy-Smolensky <s20c2007@bene.fit.ac.jp>
2022-09-05 09:14:06 +0900

    finish modal new

--- testapp/static/javascript/modal.js
+++ testapp/static/javascript/modal.js
@@ -3,8 +3,8 @@
 {
   const open = document.getElementById("modal-open");
   const container = document.getElementById("modal-container");
-  const close = document.getElementById("modal-close");
   const modalBg = document.getElementById("modal-bg");
+  const close = document.getElementById("modal-close");
   open.addEventListener("click", function () {
     container.classList.add("active");
     modalBg.classList.add("active");
